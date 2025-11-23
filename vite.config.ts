018cb45a7635070f51bd311677f0dae64a6ea630
CryptoMachine Gene <27248818+CryptoMachineGene@users.noreply.github.com>
2025-11-22 20:49:35 -0500

    chore: support both Vercel and GitHub Pages deploy targets

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGitHubPages = process.env.DEPLOY_TARGET === 'gh'
+
 export default defineConfig({
-  base: '/gene-portfolio/',
+  base: isGitHubPages ? '/gene-portfolio/' : '/',
   plugins: [react()],
   define: {
     __APP_VERSION__: JSON.stringify(new Date().toISOString()),
